4e9a050340ccd84aea99508c6e21a9ebd4ecd222
Guille <100346149@alumnos.uc3m.es>
2019-04-01 11:02:16 +0200

    Version final

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDfyIjWC2b2w_LVSCgffg9VrqctSo8aDEQ",
+  authDomain: "microp7-228f8.firebaseapp.com",
+  databaseURL: "https://microp7-228f8.firebaseio.com",
+  projectId: "microp7-228f8",
+  storageBucket: "microp7-228f8.appspot.com",
+  messagingSenderId: "253388047608"
+  };
